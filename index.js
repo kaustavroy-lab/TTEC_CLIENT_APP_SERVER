375a862486fef4192b9e5845d50e1be302431b4d
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-04-10 13:38:56 +0530

    New commit

--- index.js
+++ index.js
@@ -42,4 +42,9 @@
          effect: 'none',
          error_text: ''
       });
+      if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
+   });
+   $(window).on('load', function()
+   {
+      $('#preloader').remove();
    });
